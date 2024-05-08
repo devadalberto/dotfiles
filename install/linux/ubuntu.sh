28ca3523baa219114ebf67efa8e5151567168d0a
Jose Adalberto Valdes <jose.valdes@daimlertruck.com>
2024-05-08 14:05:08 -0700

    docker_install 005

--- install/linux/ubuntu.sh
+++ install/linux/ubuntu.sh
@@ -29,7 +29,7 @@ function install_docker() {
 	elif [[ $REPLY =~ ^[Nn]$ ]]; then
 		echo $sudoPW | sudo su -; do_housekeeping ; exit
 	else
-		install_docker()
+		exit
 	fi
 }
 #end region functions
